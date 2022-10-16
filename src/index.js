017c77e833867971254453150cd4a0113f97887d
4nzi <zm17328@josai.ac.jp>
2022-10-16 13:23:49 +0900

    add loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-export default class GLTFToonRender {
+import { parseGLB } from "./gltf-parser"
 
+export default class GLTFToonRender {
+    constructor() {
+        this.atributes = {}
+    }
+
+    loader = async (url) => {
+        const reader = new FileReader()
+        const res = await (await fetch(url)).blob()
+
+        reader.readAsArrayBuffer(res)
+        reader.onload = () => {
+            const atributes = parseGLB(reader.result)
+            Object.assign(this.atributes, atributes)
+        }
+    }
 }
