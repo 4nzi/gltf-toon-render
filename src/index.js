254a5e9b3d3f5bd33ce53f59276adde0a11b8d9e
4nzi <zm17328@josai.ac.jp>
2022-11-11 04:18:21 +0900

    make it possible to skinning

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { parseGLB } from "./parseGLB"
 import { glUtils } from "./glUtils"
 import { matIV, qtnIV } from "./minMatrix.js"
-import vertexShader from "./shaders/toon/vertexShader.glsl"
+import Bone from "./bone.js"
+import vertexShader from "./shaders/skin/vertexShader.glsl"
 import fragmentShader from "./shaders/toon/fragmentShader.glsl"
 
 export default class GLTFToonRender {
@@ -11,11 +12,6 @@ export default class GLTFToonRender {
         this.VAOs = []
         this.textures = []
         this.lightDirection = [-0.5, 0.5, 0.5]
-        this.qt = (function () {
-            const q = new qtnIV()
-            let qt = q.identity(q.create())
-            return qt
-        })()
     }
 
     setSize = (width, height) => {
@@ -27,30 +23,36 @@ export default class GLTFToonRender {
         this.lightDirection = lightDirection
     }
 
-    mouseMove = (e) => {
-        const q = new qtnIV
-        const cw = this.canvas.width
-        const ch = this.canvas.width
-        const wh = 1 / Math.sqrt(cw * cw + ch * ch);
-        let x = e.clientX - this.canvas.offsetLeft - cw * 0.5
-        let y = e.clientY - this.canvas.offsetTop - ch * 0.5
-        let sq = Math.sqrt(x * x + y * y)
-        const r = sq * 2.0 * Math.PI * wh
+    createArm = (skins) => {
+        const root = new Bone(skins[0])
+        let queue = []
 
-        if (sq != 1) {
-            sq = 1 / sq
-            x *= sq
-            y *= sq
-        }
-        q.rotate(r, [y, x, 0.0], this.qt)
+        // task in queue
+        skins.forEach(skin => {
+            skin.children?.forEach(child_id => {
+                queue.push({
+                    current: skin.id,
+                    children: child_id,
+                })
+            })
+        })
+
+        queue.forEach(elm => {
+            const childBone = new Bone(skins.find(skin => skin.id == elm.children))
+            const searchResult = []
+
+            root.searchAll(searchResult, elm.current)
+            searchResult[0].addChild(childBone)
+        })
+
+        console.log("------BoneNode-------")
+        console.log(root)
+        return root
     }
-
 
     loadGLB = async (url) => {
         const reader = new FileReader()
         const res = await (await fetch(url)).blob()
-
-        this.canvas.addEventListener('mousemove', this.mouseMove, true)
 
         reader.readAsArrayBuffer(res)
         reader.onload = async () => {
@@ -78,20 +80,15 @@ export default class GLTFToonRender {
             attLocation[0] = 0
             attLocation[1] = 1
             attLocation[2] = 2
+            attLocation[3] = 3
+            attLocation[4] = 4
 
             let attStride = []
             attStride[0] = 3
             attStride[1] = 3
             attStride[2] = 2
-
-            for (let i in this.meshes) {
-                this.VAOs.push(createVAO(
-                    [this.meshes[i].pos, this.meshes[i].nor, this.meshes[i].uv],
-                    attLocation,
-                    attStride,
-                    this.meshes[i].inx
-                ))
-            }
+            attStride[3] = 4
+            attStride[4] = 4
 
             // uniform
             let uniLocation = []
@@ -111,14 +108,22 @@ export default class GLTFToonRender {
             let mvpMatrix = m.identity(m.create())
             let invMatrix = m.identity(m.create())
 
-            // ビュー×プロジェクション座標変換行列
+            // View × projection 
             m.lookAt([0.0, 0.0, 10.0], [0, 0, 0], [0.0, 1.0, 0.0], vMatrix)
             m.perspective(45, this.canvas.width / this.canvas.height, 0.1, 100, pMatrix)
             m.multiply(pMatrix, vMatrix, pvMatrix)
 
-            // texture
-            gl.activeTexture(gl.TEXTURE0)
-            for (let i in this.meshes) {
+            this.meshes.forEach(mesh => {
+                //VAO
+                this.VAOs.push(createVAO(
+                    [mesh.attributes.pos, mesh.attributes.nor, mesh.attributes.uv,
+                    mesh.attributes.bon, mesh.attributes.wei],
+                    attLocation,
+                    attStride,
+                    mesh.attributes.inx
+                ))
+
+                // texture
                 let img = new Image()
 
                 img.onload = () => {
@@ -131,8 +136,25 @@ export default class GLTFToonRender {
 
                     this.textures.push(tex)
                 }
-                img.src = this.meshes[i].tex
-            }
+                img.src = mesh.textures.albedo
+
+                // skin
+                const skins = mesh.skins
+                const boneNode = this.createArm(skins)
+                let bones = []
+
+
+                boneNode.setJointMatrix()
+
+                for (let i in skins) {
+                    boneNode.searchJointInx(bones, i)
+                }
+
+                bones.forEach((bone, index) => {
+                    uniLocation[4] = gl.getUniformLocation(prg, `bones[${index}]`)
+                    gl.uniformMatrix4fv(uniLocation[4], false, bone)
+                })
+            })
 
             const render = () => {
                 // clear
@@ -141,11 +163,6 @@ export default class GLTFToonRender {
                 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
                 //matrix
-                var qMatrix = m.identity(m.create())
-                q.toMatIV(this.qt, qMatrix)
-
-                m.identity(mMatrix)
-                m.multiply(mMatrix, qMatrix, mMatrix)
                 m.multiply(pvMatrix, mMatrix, mvpMatrix)
 
                 // register uniform
@@ -159,12 +176,13 @@ export default class GLTFToonRender {
                     gl.bindTexture(gl.TEXTURE_2D, this.textures[i])
                     gl.bindVertexArray(this.VAOs[i])
 
-                    gl.drawElements(gl.TRIANGLES, this.meshes[i].inx.length, gl.UNSIGNED_SHORT, 0)
+                    gl.drawElements(gl.TRIANGLES, this.meshes[i].attributes.inx.length, gl.UNSIGNED_SHORT, 0)
                 }
 
                 gl.flush()
                 requestAnimationFrame(render)
             }
+
             render()
         }
     }
